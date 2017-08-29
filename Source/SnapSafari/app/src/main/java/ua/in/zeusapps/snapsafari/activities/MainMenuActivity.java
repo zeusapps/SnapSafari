de00147deaa030782e9e9576883aaa06ae2cf56c
Eugen Zagvozdkin <e.zagvozdkin@gmail.com>
2017-08-29 22:02:07 +0300

    updated layout, added navigation

--- Source/SnapSafari/app/src/main/java/ua/in/zeusapps/snapsafari/activities/MainMenuActivity.java
+++ Source/SnapSafari/app/src/main/java/ua/in/zeusapps/snapsafari/activities/MainMenuActivity.java
@@ -1,5 +1,6 @@
 package ua.in.zeusapps.snapsafari.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import de.hdodenhof.circleimageview.CircleImageView;
 import ua.in.zeusapps.snapsafari.R;
 
@@ -31,5 +33,19 @@ public class MainMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
         ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.activity_main_menu_play_button)
+    public void onPlay(){
+        Intent intent = new Intent(this, MenuActivity.class);
+
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.activity_main_menu_snap_cards_button)
+    public void onSnapCards(){
+        Intent intent = new Intent(this, SnapCardsActivity.class);
+
+        startActivity(intent);
     }
 }
