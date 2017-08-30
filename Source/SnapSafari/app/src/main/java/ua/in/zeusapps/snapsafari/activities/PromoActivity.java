9c898858ce9da5e04cf5bba74718a5602d40999a
Eugen <eugen@git.innernet>
2017-08-30 17:39:35 +0300

    updated promo activity

--- Source/SnapSafari/app/src/main/java/ua/in/zeusapps/snapsafari/activities/PromoActivity.java
+++ Source/SnapSafari/app/src/main/java/ua/in/zeusapps/snapsafari/activities/PromoActivity.java
@@ -1,15 +1,36 @@
 package ua.in.zeusapps.snapsafari.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import ua.in.zeusapps.snapsafari.R;
+import ua.in.zeusapps.snapsafari.controls.Menu;
 
 public class PromoActivity extends AppCompatActivity {
+
+    @BindView(R.id.activity_promo_title)
+    TextView title;
+    @BindView(R.id.activity_promo_description)
+    TextView description;
+    @BindView(R.id.activity_promo_valid_for)
+    TextView validFor;
+    @BindView(R.id.activity_promo_menu)
+    Menu menu;
+
+    @BindView(R.id.activity_promo_elephant)
+    ImageView elephantImage;
+    @BindView(R.id.activity_promo_promo_image)
+    ImageView promoImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_promo);
+
+        ButterKnife.bind(this);
     }
 }
