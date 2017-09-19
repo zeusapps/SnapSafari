a2d5e019803f90555c46a194ff497ef178e57dd9
Eugen <eugen@git.innernet>
2017-09-19 17:31:34 +0300

    updated main menu

--- Source/SnapSafari/app/src/main/java/ua/in/zeusapps/snapsafari/activities/MenuActivity.java
+++ Source/SnapSafari/app/src/main/java/ua/in/zeusapps/snapsafari/activities/MenuActivity.java
@@ -1,39 +1,39 @@
 package ua.in.zeusapps.snapsafari.activities;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import butterknife.OnClick;
 import de.hdodenhof.circleimageview.CircleImageView;
 import ua.in.zeusapps.snapsafari.R;
+import ua.in.zeusapps.snapsafari.common.Layout;
 
-public class MenuActivity extends AppCompatActivity {
+@Layout(R.layout.activity_main_menu)
+public class MenuActivity extends ActivityBase {
     @BindView(R.id.activity_main_menu_profile_image)
     CircleImageView _profileImage;
     @BindView(R.id.activity_main_menu_progress_bar)
     ProgressBar _progressBar;
     @BindView(R.id.activity_main_menu_info)
     TextView _infoTextView;
+    @BindView(R.id.activity_main_menu_notification_text)
+    TextView _notificationTextView;
     @BindView(R.id.activity_main_menu_play_button)
     Button _playButton;
+    @BindView(R.id.activity_main_menu_battle_button)
+    Button _buttleButton;
+    @BindView(R.id.activity_main_menu_marketplace_button)
+    Button _marketplaceButton;
     @BindView(R.id.activity_main_menu_snap_cards_button)
     Button _snapCardsButton;
     @BindView(R.id.activity_main_menu_settings_button)
     Button _settingsButton;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
-
-        ButterKnife.bind(this);
-    }
+    @BindView(R.id.activity_main_menu_notification_container)
+    FrameLayout _notificationButton;
 
     @OnClick(R.id.activity_main_menu_play_button)
     public void onPlay(){
