59b4ed098780d97a8a8a14526730ac590e191cfb
Eugen Zagvozdkin <e.zagvozdkin@gmail.com>
2017-09-24 17:40:24 +0300

    extrracted and used button styles

---
+++ Source/SnapSafari/app/src/main/java/ua/in/zeusapps/snapsafari/activities/RegisterActivity.java
@@ -0,0 +1,9 @@
+package ua.in.zeusapps.snapsafari.activities;
+
+import ua.in.zeusapps.snapsafari.R;
+import ua.in.zeusapps.snapsafari.common.Layout;
+
+@Layout(R.layout.activity_register)
+public class RegisterActivity extends ActivityBase {
+
+}
